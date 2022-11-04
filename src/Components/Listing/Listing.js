1d7bb20f8af3beee2888121b85e73295d8553964
XRachel Peng <xp2197@columbia.edu>
2022-11-04 00:18:51 -0400

    init landing page

---
+++ src/Components/Listing/Listing.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+
+
+export default function Listing() {
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Listing owner/userID
+        </Typography>
+        <Typography variant="h5" component="div">
+          Listing Title/Name
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          Listing address
+        </Typography>
+        <Typography variant="body2">
+          Listing description
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
