5c0d4eee340a20496aa7ad45047c3a215bf24e46
KenXiong123 <kenxiong0301@gmail.com>
2022-11-08 23:27:59 -0500

    add form

--- src/Components/User/UserContact.js
+++ src/Components/User/UserContact.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Sidebar from "../UserSidebar/Sidebar";
 
 function UserContact() {
+
   return (
     <div> 
         <Sidebar/> 
--- src/Components/User/UserPreference.js
+++ src/Components/User/UserPreference.js
@@ -1,17 +1,47 @@
 import React from 'react'
+import { useState } from 'react';
 import Sidebar from "../UserSidebar/Sidebar";
 import "./User.css"
 
 function UserPreference() {
-  return (
-    <div> 
-        <Sidebar/> 
-        <ul className='mainText'>
-            <li> Sleeping Time: </li>
-            <li> Wakeup Time: </li>
-        </ul>
-    </div>
-  )
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+    }
+
+    return (
+        <div>
+            <Sidebar/> 
+            <form onSubmit={handleSubmit}>
+                <label>Enter your sleeping time:
+                <input 
+                    type="text" 
+                    name="sleeping" 
+                    value={inputs.username || ""} 
+                    onChange={handleChange}
+                />
+                </label>
+                <label>Enter your wakeup time:
+                    <input 
+                    type="text" 
+                    name="wakeup" 
+                    value={inputs.age || ""} 
+                    onChange={handleChange}
+                    />
+                </label>
+                <input type="submit" />
+            </form>
+        </div>
+        
+    )
 }
 
 export default UserPreference
