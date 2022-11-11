d8ffab511b37c8d8382f3293489749a8607816e3
Xzh0u <xzh0u.sxz@gmail.com>
2022-11-10 21:10:23 -0500

    update user contact page.

--- src/Components/User/UserContact.js
+++ src/Components/User/UserContact.js
@@ -6,11 +6,31 @@ function UserContact() {
   return (
     <div>
       <Sidebar />
-      <ul className="mainText">
-        <li> Email: {UserContactData[0].emails[0]} </li>
-        <li> Phone: {UserContactData[0].phones[0]} </li>
-        <li> Address: {UserContactData[0].addresses[0]} </li>
-      </ul>
+      <div className="text-gray-500 flex-1 flex flex-col items-center">
+        <div className="flex flex-col place-items-stretch">
+          <div className="mt-1 font-normal text-lg flex ml-8">
+            <p className="p-1 font-medium text-gray-900">Email: </p>
+            <p className="hover:text-gray-900 p-1">
+              {" "}
+              {UserContactData[0].emails[0]}
+            </p>
+          </div>
+          <div className="mt-1 font-normal text-lg flex ml-8">
+            <p className="p-1 font-medium text-gray-900">Phone: </p>
+            <p className="hover:text-gray-900 p-1">
+              {" "}
+              {UserContactData[0].phones[0]}
+            </p>
+          </div>
+          <div className="mt-1 font-normal text-lg flex ml-8">
+            <p className="p-1 font-medium text-gray-900">Address: </p>
+            <p className="hover:text-gray-900 p-1">
+              {" "}
+              {UserContactData[0].addresses[0]}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
