656380280fe29376db444cdd544ac1a5bbed4940
Ruize Li <rl3250@columbia.edu>
2022-11-14 12:50:13 -0500

    nits

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-export const serverUrl =
+const USER_SERVICE_ENDPOINT =
   "https://gy8a0m85ci.execute-api.us-east-1.amazonaws.com/test";
 
-export async function getUser(id) {
+export async function getUserProfile(uid) {
+  if (uid) {
   try {
-    // const header = { "Content-Type": "application/x-www-form-urlencoded" };
-    const resp = await axios.get(`${serverUrl}/user/${id}`); //, { headers: header });
-    // return resp.data;
-    console.log(resp);
-    const firstname = resp.data.firstname;
-    return firstname;
+    const rsp = await axios.get(`${USER_SERVICE_ENDPOINT}/user/${uid}`);
+    return rsp.data;
+    // console.log(resp);
+    // const firstname = resp.data.firstname;
+    // return firstname;
   } catch (e) {
     console.error(e);
-    return id;
+  }
+  return null;
   }
 }
