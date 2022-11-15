82e26dfc997690513dbcacc9fdfdfc2dbd3fd565
XRachel Peng <xp2197@columbia.edu>
2022-11-15 11:06:20 -0500

    fixed parse error by getting rid of ?

--- src/Containers/User/UserContact.js
+++ src/Containers/User/UserContact.js
@@ -28,35 +28,15 @@ function UserContact(props) {
       <p className="hover:text-gray-900 p-1"> {val}</p>
     </div>
   );
-  // console.log(userContactData?.emails)
+
+  const email = userContactData.emails[0].address;
 
   return (
     <div>
       <Sidebar />
       <div className="text-gray-500 flex-1 flex flex-col items-center">
         <div className="flex flex-col place-items-stretch">
-          {item("Email", userContactData?.emails[0]?.address)}
-          {/* <div className="mt-1 font-normal text-lg flex ml-8">
-            <p className="p-1 font-medium text-gray-900">Email: </p>
-            <p className="hover:text-gray-900 p-1">
-              {" "}
-              {userContactData.}
-            </p>
-          </div>
-          <div className="mt-1 font-normal text-lg flex ml-8">
-            <p className="p-1 font-medium text-gray-900">Phone: </p>
-            <p className="hover:text-gray-900 p-1">
-              {" "}
-              {UserContactData[0].phones[0]}
-            </p>
-          </div>
-          <div className="mt-1 font-normal text-lg flex ml-8">
-            <p className="p-1 font-medium text-gray-900">Address: </p>
-            <p className="hover:text-gray-900 p-1">
-              {" "}
-              {UserContactData[0].addresses[0]}
-            </p>
-          </div> */}
+          {item("Email", email)}
         </div>
       </div>
     </div>
