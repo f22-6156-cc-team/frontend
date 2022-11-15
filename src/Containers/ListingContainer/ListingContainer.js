d75287689e627e594821788890d2966c24270740
XRachel Peng <xp2197@columbia.edu>
2022-11-15 15:33:11 -0500

    display all info on listing card, add pagination

--- src/Containers/ListingContainer/ListingContainer.js
+++ src/Containers/ListingContainer/ListingContainer.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
+import Grid from '@mui/material/Grid'
 import Listing from '../Listing/Listing';
 
 
 export default function ListingContainer( {listingsData} ) {
-  
     return (
-        <div className="container">
-    {/* {console.log(listingsData)} */}
+        <div style={{ padding: 30 }} className="container">
             {listingsData && listingsData.map((listing, index) => (
-                <Listing listingData={listing} key={listing.listingId} />
+                <Grid container spacing={10} justify="center">
+                    <Grid item xs={12} lg={12}>
+                        <Listing listingData={listing} key={listing.listingId} />
+                    </Grid>
+                </Grid>
             ))}
         </div>
     );
