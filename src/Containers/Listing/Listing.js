f58a2458d98218022179cd9fe2b67d149c22b44b
XRachel Peng <xp2197@columbia.edu>
2022-11-15 00:28:54 -0500

    displayed all listing info

--- src/Containers/Listing/Listing.js
+++ src/Containers/Listing/Listing.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import { Button } from '@mui/material';
 import {useNavigate} from 'react-router-dom';
 
@@ -12,20 +11,39 @@ const Listing = (props) => {
     const navigateToUser = () => {
         navigate('/user/'+userId+'/contact');
     };
+
+    const displayListing = () => {
+        const active = data.isActive ? "yes" : "no";
+        const elevator = data.hasElevator ? "yes" : "no";
+        const pet = data.isPetFriendly ? "yes" : "no";
+        const smoking = data.isSmokingFriendly ? "yes" : "no";
+        const maintenance = data.hasMaintenance ? "yes" : "no";
+        const gym = data.hasGym ? "yes" : "no";
+      return (
+        <div className="listing">
+          <h2>Listing: {data.listingName}</h2>
+          <h2>Location: {data.locationArea}</h2>
+          <h3>Address: {data.listingAddress}</h3>
+          <h3>Start date: {data.startDate}</h3>
+          <h3>End date: {data.endDate}</h3>
+          <h3>Other information and preferences:</h3>
+          <h4>Apartment size: {data.listingTotalSize} Room size: {data.listingSize}</h4>
+          <h4>Floor: {data.floor}</h4>
+          <h4>In-building elevator: {elevator}</h4>
+          <h4>In-building maintenance: {maintenance}</h4>
+          <h4>Dryer washer location: {data.dryerWasherLocation}</h4>
+          <h4>Gym: {gym}</h4>
+          <h4>Pet: {pet}</h4>
+          <h4>Smoking: {smoking}</h4>
+          <h4>Listing active: {active}</h4>
+          <h4></h4>
+        </div>
+      );
+    };
+
     const cardContent = ( data &&
         <div>
-            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                {data.listingName}
-            </Typography>
-            <Typography variant="h5" component="div">
-                {data.locationArea}
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                {data.listingAddress}
-            </Typography>
-            <Typography variant="body2">
-                {data.isActive}
-            </Typography>
+            {displayListing}
             <Button variant="outlined" onClick={navigateToUser}>
                 Contact listing owner
             </Button>
