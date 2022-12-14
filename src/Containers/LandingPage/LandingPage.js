0688066a3155f1be23b0fe567a8037ec1dc12549
Ruize Li <rl3250@columbia.edu>
2022-12-14 12:48:04 -0500

    trying to call google api

--- src/Containers/LandingPage/LandingPage.js
+++ src/Containers/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef} from "react";
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import Grid from "@mui/material/Grid";
@@ -56,13 +56,14 @@ function ListingForm({ shrinkDefault }) {
           {...(shrinkDefault && { InputLabelProps: { shrink: true } })}
         />
       ))}
-      <Grid container>
+      {/* <Grid container>
       <Button
       variant="outlined"
+      onClick={handleValidateAddr}
       >
         Validate Addr
       </Button>
-      </Grid>
+      </Grid> */}
       {[
         "currentResidentsNum",
         "totalResidentsNum",
@@ -205,10 +206,11 @@ function ListingModalContent() {
 
               data[item.id] = item.value;
             });
-            // validate address
-            const addr = data["listingAddress"];
-            const validatedAddr = APIs.getValidatedAddress(addr);
-            
+            // validate addr
+            const addr = data['listingAddress'];
+            const rsp = APIs.getValidatedAddress(addr);
+            console.log(rsp);
+            return;
 
             // TODO: edit
             let resp;
